6b19da9b3109704c21d2e3578be24c1508c41729
vanphutin <vanphutin2902@gmail.com>
2023-11-25 00:46:41 +0700

    lap trinh co so

--- SNT.cpp
+++ SNT.cpp
@@ -1,30 +1,18 @@
 //KIEM TRA SO NGUYEN TO
 
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
 
-int snt(int n);
-int main(){
-	int n;
-	printf("nhap n de liet ke cac snt ");
-	scanf("%d",&n);
-	printf("cac so nguyen to la: ");
-	for(int i=2;i<=n-1;i++){
-		if(snt(i)==1){
-			printf("%d  ",i);
-		}
-	}
-}
-
-int snt(int n){
-	if(n<2){
-		return 0;
-	} if(n==2){
+int SNT(int n){
+	for(int i=2;i<=sprt(n),i++)
+	if(n%i==0){
 		return 1;
 	}
-	for(int i=2;i<=sqrt(n);i++){
-		if(n%i==0){
-			return 0;
-		}
-	}return 1;
+} 
+
+int main(){
+	int n;
+	printf("moi nhap n:");
+	scanf("%d",n);
+	
 }
